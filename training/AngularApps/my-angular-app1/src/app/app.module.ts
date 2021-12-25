4d422d0cb72546d9c5f353f214c01c28f6bd6cc8
Jayant <jayantsarkar89@gmail.com>
2021-12-25 09:52:55 +0530

    adding products  component

--- training/AngularApps/my-angular-app1/src/app/app.module.ts
+++ training/AngularApps/my-angular-app1/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter.component';
+import { ProductsComponent } from './products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
