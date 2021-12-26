8cf48100c353b64938cf75de185368f0559de28e
Jayant <jayantsarkar89@gmail.com>
2021-12-26 21:45:04 +0530

    UI made better in products and counter

--- training/AngularApps/my-angular-app1/src/app/products.component.ts
+++ training/AngularApps/my-angular-app1/src/app/products.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'products',
-  template: `
-  <div *ngFor ="let product of products"
-  style="display:inline-block; border:1px solid red; margin:10px; 
-  padding:10px;">
-  <h2>{{product.name}}</h2>
-  <hr/>
-  <h4>{{product.price}}</h4>
-  <hr/>
-  <counter></counter>
+    selector: 'products',
+    template: `
+  <div class="row" >
+      <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-xs-12 " *ngFor ="let product of products">
+          <div class="card mt-3 mb-3"          >
+          <div class="card-header">
+              <h2>{{product.name}}</h2>
+          </div>
+ 
+          <div class="card-body">
+              <h4>{{product.price}}</h4>
+          </div>
+
+          <counter></counter>
+          </div>
+      </div>
   </div>
 
   `,
 })
 export class ProductsComponent {
-  public products = [
-    { name: 'iphone', price: 999 },
-    { name: 'ipad', price: 799 },
-    { name: 'macbook air', price: 999 },
-    { name: 'macbook pro', price: 1999 },
-  ];
+    public products = [
+        { name: 'iphone', price: 999 },
+        { name: 'ipad', price: 799 },
+        { name: 'macbook air', price: 999 },
+        { name: 'macbook pro', price: 1999 },
+    ];
 }
