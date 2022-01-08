8d4a0e232d7e43113ab9aeda90638eb99280337a
Jayant <jayantsarkar89@gmail.com>
2022-01-08 12:50:41 +0530

    added data for test2

--- training/AngularApps/my-angular-app1/src/app/test2.component.ts
+++ training/AngularApps/my-angular-app1/src/app/test2.component.ts
@@ -1,9 +1,40 @@
+import { Statement } from "@angular/compiler";
 import { Component } from "@angular/core";
 
-@Component({
-    selector:"test2",
-    template:"Test2"
-})
-export class Test2Component{
 
+interface Option2 {
+    text: string;
+    isCorrect: boolean;
+    marks: number;
+}
+
+interface Question2 {
+    statement: string;
+    options: Option2[];
+}
+
+@Component({
+    selector: "test2",
+    template: "Test2"
+})
+export class Test2Component {
+    public questions: Question2[] = [
+        {
+            statement: 'AAA',
+            options: [
+                { text: "A1", isCorrect: true, marks: 5 },
+                { text: "A2", isCorrect: false, marks: 3 }
+            ]
+        },
+        {
+            statement: 'BBB',
+            options: [
+                { text: "B1", isCorrect: true, marks: 5 },
+                { text: "B2", isCorrect: false, marks: 3 },
+                { text: "B3", isCorrect: true, marks: 5 },
+                { text: "B4", isCorrect: false, marks: 3 },
+            ]
+        },
+
+    ]
 }
