1d094913cc7a4e28641349794f8fc4c15cc53ce0
Jayant <jayantsarkar89@gmail.com>
2022-01-09 20:31:33 +0530

    added usermarks and total marks logic

--- training/AngularApps/my-angular-app1/src/app/test2.component.ts
+++ training/AngularApps/my-angular-app1/src/app/test2.component.ts
@@ -19,31 +19,74 @@ interface Question2 {
     template: `
     
         <div class="row">
-            <div class="col-12" *ngFor="let question of questions">
-                <div class="card mt2- mb-2">
-                    <div class="card-header">
-                         {{question.statement}}
+            <ng-container *ngIf="!isTestFinished">
+                <div class="col-12" *ngFor="let question of questions">
+                    <div class="card mt2- mb-2">
+                        <div class="card-header">
+                             {{question.statement}}
+                            </div>
+                        <div class="card-body" *ngFor="let option of question.options">
+                            <label>
+                                <input type="checkbox" [(ngModel)] = "option.hasUserSelected"/>
+                                {{option.text}}
+                            </label>
                         </div>
-
-                    <div class="card-body" *ngFor="let option of question.options">
-                        <label>
-                            <input type="checkbox" [(ngModel)] = "option.hasUserSelected"/>
-                            {{option.text}}
-                        </label>
                     </div>
-
                 </div>
+                
+                
+            </ng-container>
+        
+            
+            
+            <div class="col-12" *ngIf="!isTestFinished">
+            <button class="btn btn-primary" (click)="finish()">Finish</button>
             </div>
+        
+                
+            <div class="card" *ngIf="isTestFinished">
+                    <div class="card-header">
+                        Test Result
+                    </div>
+                    <div class="card-body">
+                            You have obtained {{userMarks}} out of {{totalMarks}}                            
+                    </div>
+            </div>
+            
         </div>
+        
     
     `
 })
 export class Test2Component {
+
+    public isTestFinished = false;
+    public userMarks = 0;
+    public totalMarks = 0;
+
+    public finish() {
+        for (let question of this.questions) {
+            for (let option of question.options) {
+                if (option.hasUserSelected && option.isCorrect) {
+                    this.userMarks += option.marks
+                } else if (option.hasUserSelected && !option.isCorrect) {
+                    this.userMarks -= option.marks
+                }
+                if (option.isCorrect) {
+
+                    this.totalMarks += option.marks
+                }
+            }
+        }
+        this.isTestFinished = true;
+
+    }
+
     public questions: Question2[] = [
         {
             statement: 'AAA',
             options: [
-                { text: "A1", isCorrect: true, marks: 5, hasUserSelected: true },
+                { text: "A1", isCorrect: true, marks: 5, hasUserSelected: false },
                 { text: "A2", isCorrect: false, marks: 3, hasUserSelected: false }
             ]
         },
@@ -53,7 +96,7 @@ export class Test2Component {
                 { text: "B1", isCorrect: true, marks: 5, hasUserSelected: false },
                 { text: "B2", isCorrect: false, marks: 3, hasUserSelected: false },
                 { text: "B3", isCorrect: true, marks: 5, hasUserSelected: false },
-                { text: "B4", isCorrect: false, marks: 3, hasUserSelected: true },
+                { text: "B4", isCorrect: false, marks: 3, hasUserSelected: false },
             ]
         },
 
