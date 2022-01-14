82d57b7dbc419fbd1ed358aad3d24279f3851786
jayant4 <62827014+jayant4@users.noreply.github.com>
2022-01-14 17:39:29 +0530

    Mongo db 2

--- training/mongodb/mongodb.js
+++ training/mongodb/mongodb.js
@@ -1,13 +1,9 @@
-//const {MongoClient} = require('mongodb');
-
-
-
 
 export async function connectToDatabase(client) {
 
     try {
         await client.connect();
-        
+
 
     } catch (e) {
         console.error(e);
@@ -23,6 +19,41 @@ export async function listDatabases(client) {
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
 
-export async function closeConnection(client){
+export async function closeConnection(client) {
     await client.close();
+}
+
+export async function readAll(client, dbName, collectionName) {
+    await client.db(dbName).collection(collectionName).find().toArray()
+        .then(results => {
+            console.log(results)
+        })
+        .catch(error => console.error(error))
+}
+
+
+export async function insertOne(client, dbName, collectionName, objectToInsert) {
+    await client.db(dbName).collection(collectionName).insertOne(objectToInsert).then(results => {
+        console.log(results)
+    })
+        .catch(error => console.error(error))
+}
+
+export async function updateOne(client, dbName, collectionName, objectToUpdate) {
+    await client.db(dbName).collection(collectionName).updateOne(
+        { Statement: "eee" },
+        { $set: objectToUpdate }
+    ).then(results => {
+        console.log(results)
+    })
+        .catch(error => console.error(error))
+}
+
+export async function deleteOne(client, dbName, collectionName) {
+    await client.db(dbName).collection(collectionName).deleteOne(
+        { Statement: "eee" },
+    ).then(results => {
+        console.log(results)
+    })
+        .catch(error => console.error(error))
 }
