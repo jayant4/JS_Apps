e8d92f9eae5f9840df429d9697640b36b655f611
Jayant <jayantsarkar89@gmail.com>
2022-01-29 21:39:05 +0530

    jwt for graphql

--- training/aws_practice/graphqlAPI.js
+++ training/aws_practice/graphqlAPI.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { mongoRepository } from './repository.js';
 import { graphqlHTTP } from 'express-graphql';
 import graphql from 'graphql';
+import { authenticateToken, generateAccessToken } from './jwt.js';
 
 const { GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList, GraphQLID } = graphql
 
@@ -23,6 +24,13 @@ const BookType = new GraphQLObjectType({
 const rootQuery = new GraphQLObjectType({
     name: "Query",
     fields: {
+        signup: {
+            type: GraphQLString,
+            args: { userName: { type: GraphQLString } },
+            resolve: async (parent, args) => {
+                return generateAccessToken(args.userName);
+            }
+        },
         books: {
             type: new GraphQLList(BookType),
             args: {},
@@ -39,7 +47,7 @@ const schema = new GraphQLSchema({
     query: rootQuery
 })
 
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', authenticateToken, graphqlHTTP({
     schema,
     graphiql: true
 }));
